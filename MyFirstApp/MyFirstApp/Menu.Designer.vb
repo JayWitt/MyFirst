af53b1761cbb03b5b3a4a23a116293d8195e696b
JayWitt <twizzalt@jaylan.net>
2016-10-18 10:03:12 -0500

    My First Commit.

---
+++ MyFirstApp/MyFirstApp/Menu.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Menu
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GoButton = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'GoButton
+        '
+        Me.GoButton.Location = New System.Drawing.Point(571, 534)
+        Me.GoButton.Name = "GoButton"
+        Me.GoButton.Size = New System.Drawing.Size(160, 50)
+        Me.GoButton.TabIndex = 0
+        Me.GoButton.Text = "GO !"
+        Me.GoButton.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(23, 22)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(700, 476)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Menu
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(758, 607)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.GoButton)
+        Me.Name = "Menu"
+        Me.Text = "My Awesome App"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GoButton As Button
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ MyFirstApp/MyFirstApp/Menu.vb
@@ -0,0 +1,5 @@
+﻿Public Class Menu
+    Private Sub GoButton_Click(sender As Object, e As EventArgs) Handles GoButton.Click
+        TextBox1.Text = "Now this works out great!" & vbCrLf & Now()
+    End Sub
+End Class
